6d788ac6cf104b8b46ee4e927a35f30187af725e
TerrellWhite <Terrell.White053100@gmail.com>
2018-07-19 21:58:57 -0400

    first

---
+++ GCLab5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GCLab5
+{
+    class Program
+    {
+        static void Main(string[] args)//calculate the factorial of a number
+        {
+            Console.Write("Give me a number:");
+            int input = int.Parse(Console.ReadLine());
+            long output = 1;
+            for (int i = 2; i <= input; i++)
+            {
+               output *= i;
+            }
+            Console.WriteLine(output);
+        }
+    }
+}
