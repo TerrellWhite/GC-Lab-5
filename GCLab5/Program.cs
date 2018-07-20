677d482c40d4ca30c1b4b5724de3b98dd94e74bd
TerrellWhite <Terrell.White053100@gmail.com>
2018-07-19 23:32:02 -0400

    Updated code

--- GCLab5/Program.cs
+++ GCLab5/Program.cs
@@ -10,14 +10,36 @@ namespace GCLab5
     {
         static void Main(string[] args)//calculate the factorial of a number
         {
-            Console.Write("Give me a number:");
-            int input = int.Parse(Console.ReadLine());
-            long output = 1;
-            for (int i = 2; i <= input; i++)
-            {
-               output *= i;
-            }
-            Console.WriteLine(output);
+            bool again = true;
+            do {
+            Console.Write("Give me a number between 1 and 25:");
+            int input = 0;
+                try
+                {
+                    input = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.WriteLine("you didnt put in a number");
+                }
+                if (input <= 25)
+                {
+                    long output = 1;
+                    for (int i = 2; i <= input; i++)
+                    {
+                        output *= i;
+                    }
+                    Console.WriteLine(output);
+
+                    Console.Write("Again? (Y/N)");
+                    string go = Console.ReadLine();
+                    if (go.ToLower() != "y")
+                    {
+                        again = false;
+                    }
+                
+                }
+            } while (again) ;
         }
     }
 }
